d02abc89a62bd917812e1933bb2a23c777a2b465
Christopher Gidney <chris.gidney@mccann.com>
2019-04-16 13:26:57 +0100

    Started IQTest Kata

---
+++ IQTest/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://www.codewars.com/kata/552c028c030765286c00007d/train/csharp
+
+namespace IQTest
+{
+    public class Solution
+    {
+        public static int Test(string numbers)
+        {
+            string[] split = numbers.Split(' ');
+
+            int[] nums = new int[split.Length];
+
+            for (int i = 0; i < split.Length; i++)
+            {
+                nums[i] = int.Parse(split[i]);
+            }
+
+            bool firstEven = nums[0] % 2 == 0;
+            bool secondEven = nums[1] % 2 == 0;
+            bool thirdEven = nums[2] % 2 == 0;
+
+
+
+            return 1;
+        }
+    }
+}
---
+++ IQTest/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace IQTest
+{
+    [TestFixture]
+    public class IQTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(3, Solution.Test("2 4 7 8 10"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(1, Solution.Test("1 2 2"));
+        }
+    }
+}
