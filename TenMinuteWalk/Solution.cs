fb1fa32cb3db4f2cd8d32fcbcc1eff4c4ccd7b56
Christopher Gidney <chris.gidney@mccann.com>
2019-05-15 17:24:30 +0100

    Started TenMinuteWalk

---
+++ TenMinuteWalk/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenMinuteWalk
+{
+    public class Solution
+    {
+        public static bool IsValidWalk(string[] walk)
+        {
+            var tenMinutes = 10;
+
+            var xPosition = 0;
+            var yPosition = 0;
+            var totalDistance = 0;
+
+
+            
+
+            return totalDistance == tenMinutes;
+        }
+    }
+
+    
+}
---
+++ TenMinuteWalk/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace TenMinuteWalk
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(true, Solution.IsValidWalk(new string[] { "n", "s", "n", "s", "n", "s", "n", "s", "n", "s" }), "should return true");
+            Assert.AreEqual(false, Solution.IsValidWalk(new string[] { "w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e" }), "should return false");
+            Assert.AreEqual(false, Solution.IsValidWalk(new string[] { "w" }), "should return false");
+            Assert.AreEqual(false, Solution.IsValidWalk(new string[] { "n", "n", "n", "s", "n", "s", "n", "s", "n", "s" }), "should return false");
+        }
+    }
+}
