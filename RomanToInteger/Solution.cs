34b925325bdfcedd694cac108ce81bea2bb3e1bb
Christopher Gidney <chris.gidney@mccann.com>
2019-07-05 11:22:38 +0100

    Added and completed RomanToInteger

---
+++ RomanToInteger/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToInteger
+{
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            int total = 0;
+            int nextAdd = 0;
+            char previous = '\0';
+
+            foreach (var symbol in s)
+            {
+                if (Subractions.Contains(string.Concat(previous, symbol)))
+                    total -= nextAdd;
+                else
+                    total += nextAdd;
+
+                nextAdd = Symbols[symbol];
+                previous = symbol;
+            }
+
+            total += nextAdd;
+
+            return total;
+        }
+
+
+        public static Dictionary<char, int> Symbols = new Dictionary<char, int>()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        public static HashSet<string> Subractions = new HashSet<string>()
+        {
+            { "IV" }, { "IX" }, { "XL" }, { "XC" }, { "CD" }, { "CM" }
+        };
+    }
+}
---
+++ RomanToInteger/Tests.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToInteger
+{
+    [TestFixture]
+    public class Tests
+    {
+        private readonly Solution _solution;
+
+        public Tests()
+        {
+            _solution = new Solution();
+        }
+
+        [Test]
+        public void I_returns_1()
+        {
+            var input = "I";
+            var expected = 1;
+
+            var actual = _solution.RomanToInt(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void D_returns_500()
+        {
+            var input = "D";
+            var expected = 500;
+
+            var actual = _solution.RomanToInt(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void III_returns_3()
+        {
+            var input = "III";
+            var expected = 3;
+
+            var actual = _solution.RomanToInt(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void XII_returns_12()
+        {
+            var input = "XII";
+            var expected = 12;
+
+            var actual = _solution.RomanToInt(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IV_returns_4()
+        {
+            var input = "IV";
+            var expected = 4;
+
+            var actual = _solution.RomanToInt(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IX_returns_9()
+        {
+            var input = "IX";
+            var expected = 9;
+
+            var actual = _solution.RomanToInt(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void LVIII_returns_58()
+        {
+            var input = "LVIII";
+            var expected = 58;
+
+            var actual = _solution.RomanToInt(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void MCMXCIV_returns_1994()
+        {
+            var input = "MCMXCIV";
+            var expected = 1994;
+
+            var actual = _solution.RomanToInt(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
