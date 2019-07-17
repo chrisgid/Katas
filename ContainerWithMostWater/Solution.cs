1c206f2045d9b0289d1ff8b8fc985c499867ccaa
Christopher Gidney <chris.gidney@mccann.com>
2019-07-17 16:59:46 +0100

    Started container with most water

---
+++ ContainerWithMostWater/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://leetcode.com/problems/container-with-most-water/
+
+namespace ContainerWithMostWater
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            if (height.Length == 1) return 0;
+            if (height.Length <= 2) return height.Min();
+
+            
+
+            for (int i = 0; i < height.Length; i++)
+            {
+
+            }
+
+            return height.Min();
+        }
+    }
+}
---
+++ ContainerWithMostWater/Tests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerWithMostWater
+{
+    [TestFixture]
+    public class Tests
+    {
+        Solution solution = new Solution();
+
+        [Test]
+        public void TwoLinesOneZeroLength()
+        {
+            int[] input = new int[] { 0, 3 };
+            int expected = 0;
+
+            int actual = solution.MaxArea(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TwoLines()
+        {
+            int[] input = new int[] { 2, 3 };
+            int expected = 2;
+
+            int actual = solution.MaxArea(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ThreeLinesOneZeroLength()
+        {
+            int[] input = new int[] { 2, 3, 0 };
+            int expected = 2;
+
+            int actual = solution.MaxArea(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
