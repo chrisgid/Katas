7e133173320b838ea17a4c929ef44ffe04ad7af8
Christopher Gidney <chris.gidney@live.co.uk>
2020-08-24 23:27:28 +0100

    TwoSum

---
+++ TwoSum/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace TwoSum
+{
+
+    // https://leetcode.com/problems/two-sum/
+
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var length = nums.Length;
+
+            for (var i = 0; i < length - 1; i++)
+            {
+                for (var j = i + 1; j < length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new[] { i, j };
+                    }
+                }
+            }
+
+            return new int[] { };
+        }
+    }
+}
---
+++ TwoSum/Tests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace TwoSum
+{
+    public class Tests
+    {
+        [TestCase(new [] {2, 7, 11, 15}, 9,  new [] {0, 1})]
+        [TestCase(new [] {2, 7, 11, 15}, 13, new [] {0, 2})]
+        [TestCase(new [] {2, 7, 11, 15}, 17, new [] {0, 3})]
+        [TestCase(new [] {2, 7, 11, 15}, 18, new [] {1, 2})]
+        [TestCase(new [] {2, 7, 11, 15}, 22, new [] {1, 3})]
+        [TestCase(new [] {2, 7, 11, 15}, 26, new [] {2, 3})]
+        public void TestCase1(int[] nums, int target, int[] expected)
+        {
+            // Arrange
+            var solution = new Solution();
+            
+            // Act
+            var actual = solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.Contains(expected[0], actual);
+            Assert.Contains(expected[1], actual);
+        }
+    }
+}
